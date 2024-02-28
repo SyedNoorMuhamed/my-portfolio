e61cf1f3fe3c089a2c8d718a8506ae71f8becacd
SyedNoorMuhamed <160685799+SyedNoorMuhamed@users.noreply.github.com>
2024-02-28 08:56:34 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 // import "./Header.scss";
 import { NavLink } from "react-router-dom";
 import { ImUser } from "react-icons/im";
-import { FaPhoneAlt } from "react-icons/fa";
 import { AiOutlineBars, AiOutlineClose } from "react-icons/ai";
 import { IoIosBrush, IoMdListBox } from "react-icons/io";
 import styled from "styled-components";
@@ -70,14 +69,7 @@ function Navbar() {
                 <span className="link">works</span>
               </NavLink>
             </li>
-            {/* <li>
-              <NavLink className="nav-link link" to="/contact" onClick={() => setSidebar(false)}>
-                <span className="icon">
-                  <FaPhoneAlt />
-                </span>
-                <span className="link">contact</span>
-              </NavLink>
-            </li> */}
+          
           </ul>
         </div>
       </div>
