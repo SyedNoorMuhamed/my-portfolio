35dba5631b62288af3106b7b293e06cad1d4d129
SyedNoorMuhamed <160685799+SyedNoorMuhamed@users.noreply.github.com>
2024-02-28 08:25:52 +0530

    Create Config.js
    added

---
+++ src/Config/Config.js
@@ -0,0 +1,138 @@
+import { BiCodeAlt } from 'react-icons/bi';
+import { IoBrushOutline } from 'react-icons/io5';
+
+export const SkillSet=[
+    {
+        id: "3",
+        name: "HTML 5",
+        imgsrc: "images/HTML5.png"
+    },
+    {
+        id: "4",
+        name: "CSS 3",
+        imgsrc: "images/CSS3.png"
+    },
+    {
+        id: "5",
+        name: "Javascript",
+        imgsrc: "images/javascript.png"
+    },
+    {
+        id: "6",
+        name: "Bootstrap",
+        imgsrc: "images/Bootstrap.png"
+    },
+    {
+        id: "7",
+        name: "ReactJs",
+        imgsrc: "images/React.png"
+    },
+    {
+        id: "8",
+        name: "Material-ui",
+        imgsrc: "images/Material-ui.png"
+    },
+    {
+        id: "9",
+        name: "Sass",
+        imgsrc: "images/SASS.png"
+    },
+    {
+        id: "10",
+        name: "Node Js",
+        imgsrc: "images/nodejs.png"
+    },
+    {
+        id: "11",
+        name: "Express",
+        imgsrc: "images/express.png"
+    },
+    {
+        id: "14",
+        name: "MySQL",
+        imgsrc: "images/mysql.png"
+    },
+    {
+        id: "15",
+        name: "Github",
+        imgsrc: "images/github.png"
+    }
+
+]
+
+export const ProjImg = [
+    {
+        id: "1",
+        name: "Bubble Cloud",
+        category: "react",
+        imgsrc: "projectImg/react/p_01.jpg",
+        href: "https://lively-kitten-d65e4b.netlify.app/"
+    }
+    
+    ]
+export const Experience=[
+    
+    {
+        id: "1",
+        date: "OCT 2022 - At present",
+        name: "Software Developer",
+        company: "AROOPA TECHNOLOGIES PVT LTD",
+        desc: [
+            `Developed reusable Angular components for the healthcare provider
+            application, specifically focusing on enhancing the functionality and
+            user experience across the invoice, compliance, and consultant
+            modules.`,
+            `Contributed to the development of a Flight Booking Application by
+            creating and implementing the Flight Booking Module and Hotel
+            Booking Module utilizing the React library.`,
+            `Directed the development initiatives for the Delivery Management
+            Application, architecting the delivery module with expertise in jQuery,
+            Ajax, and JavaScript technologies.`]
+    },
+    // {
+    //     id: "2",
+    //     date: "Feb 2020 - Present",
+    //     name: "Software Developer",
+    //     company: "",
+    //     desc: ["Software Developer"]
+    // },
+    
+]
+export const Education=[
+
+    {
+        id: 1,
+        date: "Jun 2018- JUN 2022",
+        name: "OASYS Institute of Technology",
+        company: "Trichy, Tamilnadu",
+        desc: "Bachelor of Engineering Degree in Computer science and Engineering OASYS Institute of Technology, Pulivalam, Trichy"
+    },
+    {
+        id: 2,
+        date: "Aug 2022 - OCT 2022",
+        name: "Java Course",
+        company: "Blue Ball Technology",
+        desc: "Completed HTML, Java, Spring Boot, Hibernate, MySql course"
+    },
+    {
+        id: 3,
+        date: "2018-2019",
+        name: "XIIth",
+        company: "Govt Boys Hr Sec School Bhuvanagiri, Cuddalore",
+        desc: ""
+    },
+    
+]
+export const Services=[
+    {
+        id: 1,
+         icon: <BiCodeAlt />,
+         name: "Web Development",
+         desc: "develop creative and responsive website layouts",
+    },
+    {   id: 2,
+        icon: <IoBrushOutline/>,
+        name: "Software Developer",
+        desc: "",
+    }
+]
